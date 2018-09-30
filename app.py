3c308a8235d1cd1144aacd4d0cb1ebc9fdae4cfa
huangyitian@gwu.edu <huangyitian@ruguoapp.com>
2018-09-30 16:49:01 +0800

    add netease_music

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import logging
-
+import json
 import configure as Conf
-from apis.link_extractor import LinkExtractor
-from flask import Flask, request
+from apis.netease_music import NeteaseMusic
+from flask import Flask, Response, request
 
 logging.basicConfig(level=Conf.LOG_LEVEL, format=Conf.LOG_FORMAT)
 app = Flask(__name__)
 
 
-@app.route('/api/link', methods=['GET'])
+@app.route('/api/search', methods=['GET'])
 def api_link_extractor():
     keyword = request.args.get('audio_name', '')
     if keyword:
-        return LinkExtractor(keyword)
-    return None
+        netease_music_res = NeteaseMusic().func_search(keyword)
+        return Response(json.dumps(netease_music_res), mimetype='application/json')
+
+    return Response('{}', mimetype='application/json')
